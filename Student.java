2a2279cccc6e6f582c90997fd6dedd95632433ba
rjayasuriya <rjaya018@uottawa.ca>
2022-10-25 20:51:36 -0400

    Create Student.java

---
+++ Student.java
@@ -0,0 +1,16 @@
+package com.example.finalproject;
+
+import java.util.ArrayList;
+
+public class Student {
+    /*
+- firstName: String
+- lastName: String
+- studentID: int
+- enrolledCourses: arrayList[Class]
+- creditedCourses: arrayList[Class]
+*/
+    public String password;
+    public int studentID;
+    public ArrayList<Class> enrolledCourses;
+}
