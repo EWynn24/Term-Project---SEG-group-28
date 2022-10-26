3ad0ae021d89b0649c2fd9ddf14dd2a8de2c61b6
rjayasuriya <rjaya018@uottawa.ca>
2022-10-25 20:51:20 -0400

    Create Instructor.java

---
+++ Instructor.java
@@ -0,0 +1,19 @@
+package com.example.finalproject;
+
+import java.util.ArrayList;
+
+public class Instructor {
+    /*
+    * - firstName: String
+- lastName: String
+- instructorID: int
+- teachingCourses: arrayList[Class]
+ * */
+    public String password;
+    public int instructorID;
+    public ArrayList<Class> teachingCourses;
+
+
+
+
+}
